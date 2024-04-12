0aa36f0b891b1d447200f727d9b6c2be91979249
Luis2o <luisomaralfarogonzalez32@gmail.com>
2024-04-12 10:28:55 -0600

    metodo para subir img y validamos al momento de guardar en el formulario

--- src/main/java/com/curso/ecommerce/controlador/ProductoControlador.java
+++ src/main/java/com/curso/ecommerce/controlador/ProductoControlador.java
@@ -4,15 +4,15 @@ import com.curso.ecommerce.modelo.Producto;
 
 import com.curso.ecommerce.modelo.Usuario;
 import com.curso.ecommerce.servicios.ProductoService;
+import com.curso.ecommerce.servicios.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -21,13 +21,16 @@ public class ProductoControlador {
 
     //Para que funcione el logger solo se importa import org.slf4j.*; y nada mas de lo contrario da error
     private final Logger LOGGER = LoggerFactory.getLogger(ProductoControlador.class);
+
+    @Autowired
+    private UploadFileService upload;
     @Autowired
     private ProductoService productoService;
 
-    @GetMapping("")
+    @GetMapping("")// se invoca cuando se hace una peticion vacia por url
     public String show(Model modelo) {
         modelo.addAttribute("prod", productoService.listaProductos());
-        return "Productos/show";
+        return "Productos/show";//nombre de la carpeta donde esta show.html
     }
 
     @GetMapping("/create")
@@ -35,11 +38,31 @@ public class ProductoControlador {
         return "Productos/create";
     }
 
-    @PostMapping("/guardar")
-    public String guardar(Producto producto) {
+    @PostMapping("/guardar")      //create.html esa variable img es del campo guardar img
+    public String guardar(Producto producto, @RequestParam("img") MultipartFile file) {
         LOGGER.info("Este es el objeto producto {}", producto);//solo sirve con ToStrinf en Producto
         Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(usuario);
+
+        //img
+        if (producto.getIdProducto() == null) {//cuando se crea un producto
+            try {
+                String nombreImagen = upload.saveImg(file);
+                producto.setImagen(nombreImagen);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            if (file.isEmpty()) {//editamos el producto pero no cambiamos la img
+                Producto p = new Producto();
+                p = productoService.buscarporID(producto.getIdProducto()).get();
+                producto.setImagen(p.getImagen());
+            }else{
+                String nombreImagen = upload.saveImg(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
         productoService.guardar(producto);
         return "redirect:/productos";//redicreccionamos al metodo show somo recibe vaci no se le pone un nombre
     }
@@ -61,4 +84,13 @@ public class ProductoControlador {
         productoService.actualiza(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Integer id) {
+        LOGGER.info("Dato a eliminar {}", id);
+        productoService.eliminar(id);
+        return "redirect:/productos";
+    }
+
+
 }
---
+++ src/main/java/com/curso/ecommerce/servicios/UploadFileService.java
@@ -0,0 +1,36 @@
+package com.curso.ecommerce.servicios;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class UploadFileService {
+
+    private String folder = "imagen";//ubicacion de las img
+
+    public String saveImg(MultipartFile file) {// guardar imagen
+        if (file.isEmpty()) {//validar si trae la img debido a que sera un null
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(folder + file.getOriginalFilename());
+                Files.write(path, bytes);
+                return file.getOriginalFilename();
+            } catch (IOException e) {
+
+            }
+        }//en caso no se quiere agregar una img desde el frame se carga una img por defaul
+        return "default.jpg";
+    }
+
+    public void eliminarImg(String nombreImg) {//elimina la img del producto eliminado
+        String url = "img//";
+        File file = new File(url + nombreImg);
+        file.delete();
+    }
+}
